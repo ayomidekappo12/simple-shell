5eb141b459325a5420992f88d8f3371182c94d02
shimeket <shimeketa@gmail.com>
2023-02-22 17:18:55 -0800

    update

--- buitin1.c
+++ buitin1.c
@@ -36,3 +36,81 @@ int unset_alias(info_t *info, char *str)
 	*equal_sign = temp_char;
 	return (ret);
 }
+
+/**
+ * set_alias - adds the alias specified by str to the alias list
+ * @info: parameter struct
+ * @str: the string alias
+ *
+ * Return: Always 0 on success, 1 on error
+ */
+int set_alias(info_t *info, char *str)
+{
+	char *equal_sign;
+
+	equal_sign = _strchr(str, '=');
+	if (!equal_sign)
+		return (1);
+	if (!*++equal_sign)
+		return (unset_alias(info, str));
+
+	unset_alias(info, str);
+	return (add_node_end(&(info->alias), str, 0) == NULL);
+}
+
+/**
+ * print_alias - prints the alias specified by node
+ * @node: the alias node
+ *
+ * Return: Always 0 on success, 1 on error
+ */
+int print_alias(list_t *node)
+{
+	char *equal_sign, *alias;
+
+	if (node)
+	{
+		equal_sign = _strchr(node->str, '=');
+		for (alias = node->str; alias <= equal_sign; alias++)
+		_putchar(*alias);
+		_putchar('\'');
+		_puts(equal_sign + 1);
+		_puts("'\n");
+		return (0);
+	}
+	return (1);
+}
+
+/**
+ * _myalias - mimics the alias builtin (man alias)
+ * @info: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
+ *  Return: Always 0
+ */
+int _myalias(info_t *info)
+{
+	int i;
+	char *equal_sign;
+	list_t *node;
+
+	if (info->argc == 1)
+	{
+		node = info->alias;
+		while (node)
+		{
+			print_alias(node);
+			node = node->next;
+		}
+		return (0);
+	}
+	for (i = 1; info->argv[i]; i++)
+	{
+		equal_sign = _strchr(info->argv[i], '=');
+		if (equal_sign)
+			set_alias(info, info->argv[i]);
+		else
+			print_alias(node_starts_with(info->alias, info->argv[i], '='));
+	}
+
+	return (0);
+}

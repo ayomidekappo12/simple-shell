2bccffe5bdec58e0bbd3a8f97517c6ee83d8a81c
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-02-23 10:59:15 -0800

    tasks

--- history.c
+++ history.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+#include <string.h>
 
 /**
  * get_history_file - gets the path to the history file
@@ -9,18 +10,18 @@
 
 char *get_history_file(info_t *info)
 {
-	char *buf, *dir;
+	char *buf, *home_dir;
 
-	dir = _getenv(info, "HOME=");
-	if (!dir)
+	home_dir = _getenv(info, "HOME=");
+	if (!home_dir)
 		return (NULL);
-	buf = malloc(sizeof(char) * (_strlen(dir) + _strlen(HIST_FILE) + 2));
+	buf = malloc(sizeof(char) * (_strlen(home_dir) + _strlen(HIST_FILE) + 2));
 	if (!buf)
 		return (NULL);
 	buf[0] = 0;
-	_strcpy(buf, dir);
-	_strcat(buf, "/");
-	_strcat(buf, HIST_FILE);
+	_strcpy(buf, home_dir);
+	_strcat (buf, "/", sizeof(buf) - strlen(buf) - 1);
+	strcat(buf, HIST_FILE);
 
 	return (buf);
 }
@@ -53,6 +54,7 @@ int write_history(info_t *info)
 	}
 	_putfd(BUF_FLUSH, fd);
 	close(fd);
+
 	return (1);
 }
 
@@ -76,20 +78,18 @@ int read_history(info_t *info)
 	free(filename);
 	if (fd == -1)
 		return (0);
+
 	if (!fstat(fd, &st))
 		fsize = st.st_size;
 	if (fsize < 2)
 		return (0);
-
 	buf = malloc(sizeof(char) * (fsize + 1));
 	if (!buf)
 		return (0);
-
 	rdlen = read(fd, buf, fsize);
 	buf[fsize] = 0;
 	if (rdlen <= 0)
 		return (free(buf), 0);
-
 	close(fd);
 	for (i = 0; i < fsize; i++)
 		if (buf[i] == '\n')
@@ -98,17 +98,15 @@ int read_history(info_t *info)
 			build_history_list(info, buf + last, linecount++);
 			last = i + 1;
 		}
-		if (last != i)
-			build_history_list(info, buf + last, linecount++);
-		free(buf);
-		info->histcount = linecount;
-		while (info->histcount-- >= HIST_MAX)
-			delete_node_at_index(&(info->history), 0);
-		renumber_history(info);
-		return (info->histcount);
-
+	if (last != i)
+		build_history_list(info, buf + last, linecount++);
+	free(buf);
+	info->histcount = linecount;
+	while (info->histcount-- >= HIST_MAX)
+		delete_node_at_index(&(info->history), 0);
+	renumber_history(info);
+	return (info->histcount);
 }
-
 /**
  * build_history_list - adds entry to a history linked list
  * @info: Structure containing potential arguments. Used to maintain

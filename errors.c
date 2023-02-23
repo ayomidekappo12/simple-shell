fa8cfc9d3671ec5e0f57e5cfe2ec6bf116345c0d
shimeket <shimeketa@gmail.com>
2023-02-22 17:31:58 -0800

    update

--- errors.c
+++ errors.c
@@ -41,3 +41,47 @@ int _eputchar(char c)
 		buf[i++] = c;
 	return (1);
 }
+
+/**
+ * _putfd - writes the character c to given fd
+ * @c: The character to print
+ * @fd: The file descriptor to write to
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putsfd - prints an input string to the given file descriptor
+ * @str: the string to be printed
+ * @fd: the file descriptor to write to
+ *
+ * Return: the number of characters printed
+ */
+int _putsfd(char *str, int fd)
+{
+	if (!str)
+		return (0);
+
+	int num_chars = 0;
+
+	for (; *str != '\0'; str++)
+	{
+		num_chars += _putfd(*str, fd);
+	}
+	return (num_chars);
+}

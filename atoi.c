27c5e1f15113bac4d8568d3d2fbdb4ec9d25d7ae
shimeket <shimeketa@gmail.com>
2023-02-22 16:42:30 -0800

    update

--- atoi.c
+++ atoi.c
@@ -26,3 +26,43 @@ int is_delim(char c, char *delim)
 	return (0);
 }
 
+/**
+ * _isalpha - checks for alphabetic characters
+ * @c: The characters to input
+ * Return: 1 or true if c is alphabetic, 0 or false otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * _atoi - converts  strings to an integer
+ * @s: the strings to be converted
+ * Return: 0 or false if no numbers in strings, converted number otherwise
+ */
+
+int _atoi(char *s)
+{
+	int i, sign = 1, result = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+	if (s[i] == '-')
+	{
+		sign *= -1;
+	}
+	else if (s[i] >= '0' && s[i] <= '9')
+	{
+		result *= 10;
+		result += (s[i] - '0');
+	}
+	if (result && (s[i + 1] < '0' || s[i + 1] > '9'))
+		break;
+	}
+
+	return (result * sign);
+}
